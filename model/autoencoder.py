b445808d310682bfc589f2287108be321188bc5e
Ilya Siganov <ilya.blan4@gmail.com>
2017-10-11 10:39:19 +0600

    add simple autoencoder

---
+++ model/autoencoder.py
@@ -0,0 +1,112 @@
+# -*- coding: utf-8 -*-
+from keras import Input
+from keras import backend as K
+from keras.callbacks import TensorBoard
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Lambda
+from keras.models import Model, Sequential
+from keras.optimizers import Adadelta
+
+from data import load_data
+
+
+def build_model():
+    """
+    Build convolution autoencoder
+    :return:
+    :rtype Sequential:
+    """
+
+    model = Sequential([
+        # Encoder
+        # 28*28*1
+        Conv2D(filters=16,
+               kernel_size=(3, 3),
+               activation='relu',
+               padding='same', input_shape=[28, 28, 1]),
+        MaxPooling2D(pool_size=(2, 2),
+                     padding='same'),
+        # 14*14*16
+
+        Conv2D(filters=8,
+               kernel_size=(3, 3),
+               activation='relu',
+               padding='same'),
+        MaxPooling2D(pool_size=(2, 2),
+                     padding='same'),
+        # 7*7*8
+
+        Conv2D(filters=8,
+               kernel_size=(3, 3),
+               activation='relu',
+               padding='same'),
+        MaxPooling2D(pool_size=(2, 2),
+                     padding='same'),
+        # 4*4*8
+
+        # Decoder
+        Conv2D(filters=8,
+               kernel_size=(3, 3),
+               activation='relu',
+               padding='same'),
+        UpSampling2D(size=(2, 2)),
+        # 8*8*8
+
+        Conv2D(filters=8,
+               kernel_size=(3, 3),
+               activation='relu',
+               padding='same'),
+        UpSampling2D(size=(2, 2)),
+        # 16*16*8
+
+        Conv2D(filters=16,
+               kernel_size=(3, 3),
+               activation='relu',
+               padding='valid'),
+        UpSampling2D(size=(2, 2)),
+        # 32*32*16
+
+        Conv2D(filters=1,
+               kernel_size=(3, 3),
+               activation='sigmoid',
+               padding='same')
+        # 32*32*1
+    ])
+
+    model.summary()
+    return model
+
+
+def train(model, x_train, y_train, x_test, y_test):
+    """
+
+    :param model:
+    :type model: Sequential
+    :param x_train:
+    :param y_train:
+    :param x_test:
+    :param y_test:
+    :return:
+    """
+    model.compile(optimizer=Adadelta(), loss=K.binary_crossentropy)
+    model.fit(x=x_train,
+              y=x_train,
+              epochs=50,
+              batch_size=128,
+              shuffle=True,
+              validation_data=(x_test, x_test),
+              callbacks=[TensorBoard(
+                  log_dir="/tmp/tensorflow/autoencoder",
+                  write_images=False,
+                  histogram_freq=0,
+                  batch_size=128
+              )])
+
+
+def main():
+    x_train, y_train, x_test, y_test = load_data()
+    model = build_model()
+    train(model, x_train, y_train, x_test, y_test)
+
+
+if __name__ == '__main__':
+    main()
